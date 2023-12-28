15f965f13b941a5bc2794248cbed5e54c13f4ef7
Min Seok Kim <minseok.kim@brain.snu.ac.kr>
2023-12-28 15:42:18 +0900

    :sparkles: Add features for computing speed through DLC coordinates.

---
+++ motion/extractor.py
@@ -0,0 +1,15 @@
+import os
+
+import pandas as pd
+
+
+def extract(path):
+    # Get file extension
+    _, extension = os.path.splitext(path)
+
+    # Extract file
+    if extension == 'xlsx':
+        data = pd.read_excel(path)
+    if extension == 'csv':
+        data = pd.read_csv(path)
+    return data
