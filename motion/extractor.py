6726d353b0104c695a1bfffadfff44d4c6550f17
Min Seok Kim <minseok.kim@brain.snu.ac.kr>
2023-12-28 16:12:57 +0900

    :bug: Fix extension error.

--- motion/extractor.py
+++ motion/extractor.py
@@ -4,12 +4,15 @@ import pandas as pd
 
 
 def extract(path):
+    # Output
+    data = []
+
     # Get file extension
     _, extension = os.path.splitext(path)
 
     # Extract file
-    if extension == 'xlsx':
+    if extension == '.xlsx':
         data = pd.read_excel(path)
-    if extension == 'csv':
+    if extension == '.csv':
         data = pd.read_csv(path)
     return data
