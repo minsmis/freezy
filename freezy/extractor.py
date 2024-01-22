30ea80bf799f1f06c256942e2636774aa5c3ef42
Min Seok C. Kim <minseok.kim@brain.snu.ac.kr>
2024-01-22 11:13:33 +0900

    :sparkles: Add 'read_bodyparts' function.

--- freezy/extractor.py
+++ freezy/extractor.py
@@ -3,7 +3,7 @@ import os
 import pandas as pd
 
 
-def extract(path):
+def extract_data(path):
     # Output
     data = []
 
@@ -16,3 +16,18 @@ def extract(path):
     if extension == '.csv':
         data = pd.read_csv(path)
     return data
+
+
+def read_bodyparts(data):
+    # Parameters
+    # data [DataFrame]: Return of 'extract_data'.
+    # Return
+    # bodyparts [list]: Name of bodyparts in the data.
+
+    # Read data head
+    head = data.head()
+
+    # Read columns containing bodyparts
+    bodyparts = list(head.columns[1:])  # 'bodyparts' in index=0
+
+    return bodyparts
